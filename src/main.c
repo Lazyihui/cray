fa611e24f290710251ce5e11ae097169e2399fa0
包子 <chengbaozi66@qq.com>
2023-11-03 21:35:09 +0800

    circle

--- src/main.c
+++ src/main.c
@@ -6,125 +6,91 @@
 #include "RoleEntity_Move.h"
 #include "stdlib.h"
 #include "time.h"
+#define WIDTH 50 // 定义一个常量
+#define HEIGHT 3
 
-int Big(int a , int b ){
-    if (a>b){
-        return a;
-    }else{
-        return b;
-    }
-}
-
-bool IsPointInsideRect(Vector2 pos, Vector2 rectanglePoint,Vector2 rectangleSize){
-    if (pos.x>rectanglePoint.x && pos.x<rectanglePoint.x+rectangleSize.x && pos.y>rectanglePoint.y && pos.y<rectanglePoint.y+rectangleSize.y){
+bool IsPointInsideRect(Vector2 pos, Vector2 rectanglePoint, Vector2 rectangleSize) {
+    if (pos.x > rectanglePoint.x && pos.x < rectanglePoint.x + rectangleSize.x && pos.y > rectanglePoint.y &&
+        pos.y < rectanglePoint.y + rectangleSize.y) {
         return true;
-    }else{
+    } else {
         return false;
     }
-
 }
 
-bool ISPointsideCircle(Vector2 pos, Vector2 circlepoint, float circleradius){
-   
-    if ( (pos.x-circlepoint.x)*(pos.x-circlepoint.x)+(pos.y-circlepoint.y)*(pos.y-circlepoint.y)<=circleradius*circleradius){
+bool ISPointsideCircle(Vector2 pos, Vector2 circlepoint, float circleradius) {
+
+    if ((pos.x - circlepoint.x) * (pos.x - circlepoint.x) + (pos.y - circlepoint.y) * (pos.y - circlepoint.y) <=
+        circleradius * circleradius) {
         return true;
-    }else{
+    } else {
         return false;
     }
-
 }
 
+int GetIndex(int indexwidth, int x, int y) {
+    return x + indexwidth * y;
+} //*arr   or  arr[]   数组
 
-
-
-
-
-
-
-
-int GetIndex(int *arr , int arrLines, int arrCountPerLine,int VisitLineIndex, int visitCoulumnIndex ){
-    int Index;
-    return Index;
+void DrawArr(int *arr, int width, int height) {
+    for (int x = 0; x < width; x += 1) {
+        for (int y = 0; y < height; y += 1) {
+            int index = GetIndex(width, x, y);
+            if (arr[index] == 0) {
+                DrawPixel(x, y, RED);
+            } else if (arr[index] == 1) {
+                DrawPixel(x, y, YELLOW);
+            } else if (arr[index] == 2) {
+                DrawPixel(x, y, BLUE);
+            }
+        }
+    }
 }
 
 int main(void) {
     InitWindow(800, 450, "cyh");
-        int arr[100000] ={0};
-        int i,j;
-        arr[500] = 1;
-    RoleEntity role = {.moveAxis = (Vector2){0},
-                       .pos = {.x = 40, .y = 20},
-                       .height = 50,
-                       .width = 50,
-                       .moveSpeed = 100,
-                       .color = GREEN};
-    RoleEntity role1 = {.moveAxis = (Vector2){0},
-                        .pos = {.x = 40, .y = 320},
-                        .height = 50,
-                        .width = 50,
-                        .moveSpeed = 150,
-                        .color = RED};
-    RoleEntity role2 = {.moveAxis = {0}, .color = RED, .height = 20, .width = 40, .pos = {.x = 50, .y = 50}};
-            srand(time(NULL));
-    
+
+    int arr[WIDTH * HEIGHT] = {0}; // 1 1 1 1 0
+
+    int index = 0;
+
+    SetTargetFPS(60);
+
+    int status = 0;
 
     while (!WindowShouldClose()) {
-        // BeginDrawing();
-        ClearBackground(BLACK);
-        float dt = GetFrameTime(); //
-        RoleEntity_Draw(&role1);
-        RoleEntity_Draw(&role);
-        // RoleEntity_DrawB(&Bullet);
-        RoleEntity_Input(&role1);
-        RoleEntity_InputG(&role);
-        // RoleEntity_InputB(&Bullet);
-      RoleEntity_Move(&role1, dt);
-        RoleEntity_MoveG(&role, dt);
-        // RoleEntity_MoveB(&Bullet,dt);
-        TextFormat("Delta Time : %f ", dt);
-        DrawText("hh",0,0,10,YELLOW);
-        DrawRectangle(50, 50, 45, 20, RED);
-        DrawText("press", 50, 50, 15, BLACK);
 
+        float dt = GetFrameTime();
+
+        BeginDrawing();
         Vector2 pos = GetMousePosition();
-        bool isleftdown = IsMouseButtonPressed( MOUSE_BUTTON_LEFT);
-        Vector2 rectangle= {.x=50,.y=50};
-
-        bool isinside = IsPointInsideRect(pos,rectangle,(Vector2){.x=45,.y=20});
-
-        if (isinside && isleftdown) {
-           
-        }
-        DrawCircle(80,80,10,YELLOW);
-        DrawText("press",80,80,15,BLACK);
-        bool isinsidecircle = ISPointsideCircle(pos, (Vector2){.x=80,.y=80,},10);
-
-        if(isinsidecircle && isleftdown ){
-            DrawCircle(80,120,20,RED);
-        }
-       
-
-        // int Index =GetIndex(arr[500],500,);
-        // for(int i=0; i<100000 ; i++ ){
-        //     if(arr[i]==0){
-        //         continue;
-        //     }else{
-                
-        //         int i = rand ();
-        //     }
         
-        // };
+
+        // YELLOW
+        if (status == 0) {
+            arr[index] = 1;
+            index += 1;
+            if (index >= WIDTH * HEIGHT) {
+                status = 1;
+                index = WIDTH * HEIGHT -1;
+            }
+        } else if (status == 1) {
+            arr[index] = 2;
+            index -= 1;
+            if (index < 0) {
+                status = 0;
+                index = 0;
+            }
+        }
         
+        // BLUE
+
+        DrawArr(arr, WIDTH, HEIGHT);
 
         EndDrawing();
-    
-
-        
     }
 
     CloseWindow();
 
     return 0;
 }
-
-// 按钮   方块加字 然后 鼠标找到
